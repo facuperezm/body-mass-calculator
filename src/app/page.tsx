9a1f3fcaa5d4ef55bd4969e39d4b773cd1d6cae2
facuperezm <facundoperezmontalvo@gmail.com>
2023-05-16 00:24:36 -0300

    working on ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,23 +10,36 @@ export default function Home() {
   const [value, setValue] = React.useState("metric");
   const [measures, setMeasures] = React.useState(INITIAL_STATE);
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    console.log(measures);
-  }
+  function calculateBMI(weight: number, height: number) {
+    let bmi;
 
-  function calculateBMI(weight, height) {
-    // Ensure height is in meters
-    let heightInMeters = height / 100;
-
-    // Calculate BMI
-    let bmi = weight / (heightInMeters * heightInMeters);
-
+    if (value === "metric") {
+      let heightInMeters = height / 100;
+      bmi = weight / (heightInMeters * heightInMeters);
+    } else {
+      let heightInInches = height * 12;
+      bmi = (weight / (heightInInches * heightInInches)) * 703;
+    }
     return bmi.toFixed(1);
   }
 
+  function calculateIdealWeightRange(height: number) {
+    let lowerRange;
+    let upperRange;
+    if (value === "metric") {
+      let heightInMeters = height / 100;
+      lowerRange = 18.5 * (heightInMeters * heightInMeters);
+      upperRange = 24.9 * (heightInMeters * heightInMeters);
+    } else {
+      let heightInInches = height * 12;
+      lowerRange = 18.5 * (heightInInches * heightInInches);
+      upperRange = 24.9 * (heightInInches * heightInInches);
+    }
+    return [lowerRange.toFixed(2), upperRange.toFixed(2)];
+  }
+
   return (
-    <main className="text-white bg-black max-w-7xl">
+    <main className="text-gray-900 bg-teal-100 max-w-7xl">
       {/* logo */}
       <header className="flex flex-col items-center justify-center py-2">
         <div>
@@ -38,10 +51,12 @@ export default function Home() {
             overall health and well-being.
           </p>
         </div>
-        <div>
-          <p>Enter your details below</p>
-          <form onSubmit={handleSubmit}>
-            <fieldset>
+        <div className="p-3 bg-white rounded-lg">
+          <p className="text-lg font-semibold text-gray-900">
+            Enter your details below
+          </p>
+          <form className="" onSubmit={(e) => e.preventDefault()}>
+            <fieldset className="flex gap-3">
               <legend>Value</legend>
 
               <input
@@ -69,40 +84,54 @@ export default function Home() {
               />
               <label htmlFor="imperial">Imperial</label>
             </fieldset>
-            <label htmlFor="height">
-              <input
-                id="height"
-                value={measures.height}
-                onChange={(event) => {
-                  setMeasures(
-                    (measures) =>
-                      ({
-                        ...measures,
-                        height: Number(event.target.value),
-                      } as typeof measures)
-                  );
-                }}
-              />
-            </label>
-            <label htmlFor="weight">
-              <input
-                id="weight"
-                value={measures.weight}
-                onChange={(event) => {
-                  setMeasures(
-                    (measures) =>
-                      ({
-                        ...measures,
-                        weight: Number(event.target.value),
-                      } as typeof measures)
-                  );
-                }}
-              />
-            </label>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="height">
+                <input
+                  className="px-2 py-1 text-lg border border-gray-300 rounded-md"
+                  id="height"
+                  value={measures.height}
+                  onChange={(event) => {
+                    setMeasures(
+                      (measures) =>
+                        ({
+                          ...measures,
+                          height: Number(event.target.value),
+                        } as typeof measures)
+                    );
+                  }}
+                />
+              </label>
+              <label htmlFor="weight">
+                <input
+                  className="px-2 py-1 text-lg border border-gray-300 rounded-md"
+                  id="weight"
+                  value={measures.weight}
+                  onChange={(event) => {
+                    setMeasures(
+                      (measures) =>
+                        ({
+                          ...measures,
+                          weight: Number(event.target.value),
+                        } as typeof measures)
+                    );
+                  }}
+                />
+              </label>
+            </div>
           </form>
-          <div>
-            <p>Your BMI is...</p>
-            <span>{calculateBMI(measures.weight, measures.height)}</span>
+          <div className="p-2 text-white bg-blue-600 rounded-md">
+            <p className="">Your BMI is...</p>
+            <span className="text-5xl font-semibold ">
+              {calculateBMI(measures.weight, measures.height)}
+            </span>
+            <p className="">
+              Your BMI suggests you're a healthy. Your ideal weigth is between{" "}
+              {calculateIdealWeightRange(measures.height).map((weight) => (
+                <span key={weight} className="mr-2 font-semibold">
+                  {weight} kg
+                </span>
+              ))}
+            </p>
           </div>
         </div>
       </header>
